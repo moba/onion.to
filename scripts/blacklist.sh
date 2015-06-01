1c91ec9cbed86d76f39a4d3f30a93375e96ce92f
Marc <marc-git@crazyhaze.de>
2015-06-01 22:01:54 +0200

    add blacklist.sh

---
+++ scripts/blacklist.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+BLACKLIST="blacklist.txt"
+
+HASH=`echo -e "$1\c" | md5sum | sed 's/\s.*$//'`
+
+if [ -n "$1" ]; then
+  echo "--------------------"
+  echo "URL: $1"
+  echo "HASH: $HASH"
+  echo "--------------------"
+  echo "Hash will be stored in '$BLACKLIST'"
+  echo -e $HASH >> $BLACKLIST
+else
+  echo ">> I need an address <<"
+  echo "\$ $0 duskgytldkxiuqc6.onion"
+fi
